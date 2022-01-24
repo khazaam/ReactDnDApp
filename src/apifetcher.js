0553a5a4b48c0667565a02e42a781ba02675bef9
Ville <villeuhlgren@gmail.com>
2022-01-24 17:06:24 +0200

    Notes for school project

--- src/apifetcher.js
+++ src/apifetcher.js
@@ -1,7 +1,12 @@
 //TODO api-fetcer either using reacts own system, or that plugin
 //this is a api-fetcher thingie
+//make custom hook
 import axios from 'axios';
 
+//Add the specific content into the the get etc
+//such as names etc
+//how the arim works, and or etc
+//tiedonhakujuttuja lisää
 export default function ApiFetcher(){
 
     axios.get('https://www.dnd5eapi.co/api/monsters')
