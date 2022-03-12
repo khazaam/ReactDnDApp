c19fe625ff398ff9b5c9f1832b272f024e680ca8
Ville <villeuhlgren@gmail.com>
2022-03-12 18:52:51 +0200

    debuggin the api call, problem with the useeffect

--- src/apifetcher.js
+++ src/apifetcher.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 //TODO api-fetcer either using reacts own system, or that plugin
 //this is a api-fetcher thingie
 //make custom hook
@@ -16,21 +17,42 @@ const apiUrli = "https://www.dnd5eapi.co/api/monsters";
 
 
 
-export default function ApiFetcher(){
+function ApiFetcher(){
 
-    const[apiData, setApiData] = useState({});
+    
+    const[setApiData] = useState({});
+
+      useEffect(() => {
+        AxiData();
+        //some problem with this
+      }, []);
+
+    
 
     // old quick
+    
     axios.get(apiUrli)
     .then(response => {
 
-        console.log(response);
+        console.log(response, "eka");
 
     })
+    
     //
+    
+    //old
+    const AxiData = async () => {
+        const response = await axios.get(apiUrli, "toka");
+        setApiData(response.data);
+        console.log(response.data);
 
-    useEffect(() => {
+    };
 
-    })
+    //new, broken part still
+
+    //
+    
 
 }
+
+export default ApiFetcher;
